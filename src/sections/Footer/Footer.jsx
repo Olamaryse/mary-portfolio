29cef975580e9813989324d36d87afa6dee6d1ce
Mary Popoola <popoolamarimar@gmail.com>
2024-12-09 19:27:09 +0000

    first commit

---
+++ src/sections/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import styles from './FooterStyles.module.css';
+
+function Footer() {
+  return (
+    <section id="footer" className={styles.container}>
+      <p>
+        &copy; 2024 Mary Popoola. <br />
+      </p>
+    </section>
+  );
+}
+
+export default Footer;
